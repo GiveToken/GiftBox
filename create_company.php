777037bee366c83ee11a0d9df4dad2041263ab34
Shrey Desai <shreydesai@me.com>
2016-07-11 10:13:00 -0700

    designed glassdoor modal

--- create_company.php
+++ create_company.php
@@ -348,6 +348,14 @@ require __DIR__.'/header.php';
               USE EXISTING COMPANY
             </paper-button>
           </div>
+          <div class="button-container">
+            <glassdoor-scraper></glassdoor-scraper>
+          </div>
+          <div class="button-container">
+            <paper-button raised onclick="initGlassdoor()" id="glassdoor-init">
+              IMPORT FROM GLASSDOOR
+            </paper-button>
+          </div>
           <!--
           <div class="button-container">
             <paper-button raised onclick="initLinkedIn()" id="linkedin-scrape-button">
@@ -395,6 +403,18 @@ require __DIR__.'/header.php';
       </div>
     </paper-dialog>
 
+    <paper-dialog class="recruiting-dialog" id="glassdoor-dialog" modal>
+      <h2>Import company information from Glassdoor</h2>
+      <paper-input id="glassdoor-cname" label="Enter your company's name" autofocus></paper-input>
+      <div class="buttons">
+        <paper-button id="glassdoor-submit-button" class="dialog-button" onclick="processGlassdoor()">Submit</paper-button>
+        <paper-button id="glassdoor-cancel-button" class="dialog-button" onclick="cancelGlassdoor()" dialog-dismiss>Cancel</paper-button>
+      </div>
+      <div id="glassdoor-progress">
+        <paper-progress indeterminate></paper-progress>
+      </div>
+    </paper-dialog>
+
     <paper-dialog class="recruiting-dialog" id="video-dialog" modal>
         <h2>Upload video from web address</h2>
         <paper-input id="video-dialog-url" label="Paste video embed URL here" autofocus></paper-input>
@@ -423,6 +443,7 @@ require __DIR__.'/header.php';
     <script src="js/create_common.min.js?v=<?php echo VERSION;?>"></script>
     <script src="js/create_recruiting.min.js?v=<?php echo VERSION;?>"></script>
     <script src="js/linkedin-scraper.min.js?v=<?php echo VERSION;?>"></script>
+    <script src="js/glassdoor-scraper.min.js?v=<?php echo VERSION;?>"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script>
     /**
