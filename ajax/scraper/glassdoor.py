acf9623b87f4193176c0cb9444e51bbf2ca27879
Shrey Desai <shreydesai@me.com>
2016-07-12 11:18:00 -0700

    wrote script to download logos

---
+++ ajax/scraper/glassdoor.py
@@ -0,0 +1,21 @@
+import requests
+import shutil
+import json
+import sys
+import os
+
+key = sys.argv[1]
+os.chdir('../../public/uploads')
+json_file = 'tempurl-{}.json'.format(key)
+
+if json_file in os.listdir():
+    with open(json_file, 'r') as f:
+        temp = json.load(f)
+    url = temp['url']
+
+    headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36"}
+    res = requests.get(url, headers=headers, stream=True)
+    with open('glassdoor-{}.png'.format(key), 'wb') as f:
+        shutil.copyfileobj(res.raw, f)
+
+    os.remove(json_file)
