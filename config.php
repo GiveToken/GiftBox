0268cab4d5404553ad087d98ee823c864f295c72
Bradley Wogsland <bradley@wogsland.org>
2016-11-07 10:46:37 -0600

    landing only

--- config.php
+++ config.php
@@ -21,12 +21,8 @@ require_once __DIR__.'/util.php';
 
 // Determine environment & fix URL as needed
 $server = $_SERVER['SERVER_NAME'] ?? null;
-if ('gosizzle.io' == strtolower($server) || 'givetoken.com' == strtolower($server) || 'www.givetoken.com' == strtolower($server)) {
-    $url = 'https://www.gosizzle.io'.$_SERVER['REQUEST_URI'];
-    header("Location: $url ", true, 301);
-}
 if (!defined('ENVIRONMENT')) {
-    define('ENVIRONMENT', 'www.gosizzle.io' == $server ? 'production' : ('127.0.0.1' == $_SERVER['SERVER_ADDR'] ? 'local' : 'development'));
+    define('ENVIRONMENT', 'production');
 }
 
 
@@ -52,13 +48,6 @@ if (ENVIRONMENT != 'local') {
 $prefix = "http://";
 $use_https = false;
 $socket = null;
-if (ENVIRONMENT == 'production') {
-    $stripe_secret_key = 'sk_live_MuUj2k3WOTpvIgw8oIHaON2X';
-    $stripe_publishable_key = 'pk_live_AbtrrWvSZZCvLYdiLohHC2Kz';
-} else {
-    $stripe_secret_key = 'sk_test_RTcVNjcQVfYNiPCiY5O9CevV';
-    $stripe_publishable_key = 'pk_test_Gawg5LhHJ934MPXlvglZrdnL';
-}
 if (isset($_SERVER['HTTPS'])) {
     if ($_SERVER['HTTPS'] === "on") {
         $prefix = "https://";
@@ -86,14 +75,6 @@ if (!defined('APP_URL')) {
     define('APP_URL', $prefix.$server.'/');
 }
 
-// connect to database
-include __DIR__.'/config/credentials.php';
-$mysqli = new mysqli($mysql_server, $user, $password, $database);
-if ($mysqli->connect_error) {
-    die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
-}
-new Connection($mysqli);
-
 // start session
 session_start();
 
@@ -102,19 +83,4 @@ if (isset($_COOKIE, $_COOKIE['visitor'])) {
     $visitor_cookie = $_COOKIE['visitor'];
 } else {
     $visitor_cookie = substr(md5(microtime()), rand(0, 26), 20);
-}
-// set or reset cookie to expire in a year
-setcookie('visitor', $visitor_cookie, time()+60*60*24*365);
-$webRequest = new WebRequest();
-$webRequest->visitor_cookie = $visitor_cookie;
-if (isset($_SESSION, $_SESSION['user_id'])) {
-    $webRequest->user_id = $_SESSION['user_id'];
-}
-if (isset($_SERVER)) {
-    $webRequest->host = ($_SERVER['HTTP_HOST'] ?? '');
-    $webRequest->user_agent = ($_SERVER['HTTP_USER_AGENT'] ?? '');
-    $webRequest->uri = ($_SERVER['REQUEST_URI'] ?? '');
-    $webRequest->remote_ip = ($_SERVER['REMOTE_ADDR'] ?? '');
-    $webRequest->script = ($_SERVER['SCRIPT_NAME'] ?? '');
-    $webRequest->save();
 }
